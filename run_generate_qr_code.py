b53843ae3f36d76559b523ccc4489bab326353e8
HelmiHalmouch <helmirealmadrid@gmail.com>
2021-03-22 20:39:38 +0100

    add run the code and the resutl of test

---
+++ run_generate_qr_code.py
@@ -0,0 +1,11 @@
+'''
+Project : Divers helps
+Authors: GHANMI Helmi 
+Date :  21 March 2021
+'''
+from generate_qr_code import generate_qr_code
+
+site_uri = "https://www.researchgate.net/profile/Ghanmi-Helmi"
+qr_output_file_name = 'web_site_ghanmi_helmi'
+generate_qr_code(site_uri, qr_output_file_name)
+
